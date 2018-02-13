45fa85684cd5406e626e1771862886507d0e22dd
brunats <silvatavares.bruna@gmail.com>
2018-02-13 00:03:30 -0200

    adicionando configurações api instagram

--- hello.rb
+++ hello.rb
@@ -1,10 +1,24 @@
 # --
+# pacotes necessários
+# => gem install sinatra thin haml
+# => gem install instagram
 # http://192.168.0.102:4567/
 
-require 'sinatra'
-require 'Instagram'
+require "sinatra"
+require "instagram"
+
 #coding: utf-8
 set :bind, '0.0.0.0'
+
+#Config Instagram
+#
+CALLBACK_URL = "http://192.168.0.105:4567/oauth/callback"
+
+Instagram.configure do |config|
+  config.client_id = "c7dceb3cadb242fc86c8acf894e444e2"
+  config.client_secret = "99a27b9b9f3346d9b408f462e1d5ca7c"
+  config.scope = "basic"
+end
 
 get '/' do
   @titulo = "Instagram API"
@@ -18,8 +32,12 @@ get '/sobre' do
   haml :sobre
 end
 
-get '/conecte' do
-	@titulo = "Instagram API"
-  	@subtitulo = "O projeto mais top"
-  	haml :conecte
+get '/oauth/connect' do
+  redirect Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
+end
+
+get "/oauth/callback" do
+  response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
+  session[:access_token] = response.access_token
+  redirect "/"
 end
