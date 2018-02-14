1b970194e0fb6a4375ca327f97b10d9fb80d3c73
brunats <silvatavares.bruna@gmail.com>
2018-02-14 01:16:05 -0200

    arquivos haml traduzidos para erb

--- hello.rb
+++ hello.rb
@@ -20,7 +20,7 @@ CALLBACK_URL = "http://192.168.0.105:4567/oauth/callback"
 Instagram.configure do |config|
   config.client_id = "c7dceb3cadb242fc86c8acf894e444e2"
   config.client_secret = "99a27b9b9f3346d9b408f462e1d5ca7c"
-  config.scope = "likes comments public_content follower_list comments relationships"
+  config.scope = "basic public_content"
   # For secured endpoints only
   #config.client_ips = '<Comma separated list of IPs>'
 end
@@ -28,13 +28,7 @@ end
 get '/' do
   @titulo = "Instagram API"
   @subtitulo = "O projeto mais top"
-  haml :index
-end
-
-get '/sobre' do
-  @titulo = "Instagram API"
-  @subtitulo = "O projeto mais top"
-  haml :sobre
+  erb :index
 end
 
 get '/oauth/connect' do
@@ -44,21 +38,24 @@ end
 get "/oauth/callback" do
   response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
   session[:access_token] = response.access_token
-  redirect "/"
+  redirect "/home"
+end
+
+get '/home' do
+  client = Instagram.client(:access_token => session[:access_token])
+  user = client.user
+  @titulo = "Instagram API"
+  @subtitulo = "Bem vindo "+user.username
+  erb :home
 end
 
 get "/user_recent_media" do
   client = Instagram.client(:access_token => session[:access_token])
   user = client.user
-  html = "<h1>As 5 primeiras fotos de #{user.username}</h1>"
-  $i = 1
-  for media_item in client.user_recent_media
-    $i+=1
-    html << "<div style='float:left;'><img src='#{media_item.images.thumbnail.url}'><br/></div>"
-    if $i > 5 then
-      break
-    end
-  end
-  html << "<a href='/'>Voltar</a>"
-  html
+  @titulo = "Instagram API"
+  erb :fotos
+end
+
+get "/logout" do
+
 end
